1e4b741bb5dc354da1fd6263bf0f0f307cd3bb29
Jorge Arco <jorge.arcoma@gmail.com>
2017-04-17 03:45:48 +0200

    Update fake_test.go

--- cmd/fake_test.go
+++ cmd/fake_test.go
@@ -10,7 +10,7 @@ import (
 func TestFake(t *testing.T) {
 	repo := post.GetRepository()
 
-	p :=repo.Call("1");
+	p := repo.Call("1");
 
 	if (p.Id != 1) {
 		log.Fatal(fmt.Sprintf("Post.Id must be 1 and its %d", p.Id))
